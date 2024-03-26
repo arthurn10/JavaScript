2a9848027399cd0bc85695b1ab16e188f897affb
arthurn10 <arthur.nogueira01@fatec.sp.gov.br>
2024-03-25 21:04:44 -0300

    .

---
+++ exercicios/ex008.js
@@ -0,0 +1,12 @@
+
+var vel = 99
+
+console.log(`Sua velocidade é de ${vel} km/h`)
+
+if (vel > 90){
+    console.log ( `Você ultrapassou o limite de velocidade e será multado!`)
+} 
+     console.log(`Obedeça as sinalizações e use o cinto de segurança!`)
+
+
+        
