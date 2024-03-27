4dbfba33e3cd7f43f811188c32f7c6a2495af288
arthurn10 <arthur.nogueira01@fatec.sp.gov.br>
2024-03-26 21:07:31 -0300

    .

---
+++ exercicios/excase012.js
@@ -0,0 +1,37 @@
+var hj = new Date()
+
+var data = hj.getDay()
+
+switch(data){
+    case 0:
+        console.log("Hoje é domingo")
+        breack
+
+    case 1: 
+        console.log("Hoje é segunda")
+        breack
+
+    case 2: 
+        console.log("Hoje é terça")
+        breack
+    
+    case 3:
+        console.log("Hoje é quarta")
+        breack
+
+    case 4:
+        console.log("Hoje é quinta")
+        breack
+
+    case 5:
+        console.log("Hoje é sexta")
+        breack
+
+    case 6:
+        console.log("Hoje é sábado")
+        breack
+    
+    default:
+        console.log('[ERRO]')
+
+}
