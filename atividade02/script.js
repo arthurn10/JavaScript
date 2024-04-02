d6e484b9885fc54ad35d7959d1f4f93d1c9f33a1
arthurn10 <arthur.nogueira01@fatec.sp.gov.br>
2024-04-02 20:01:01 -0300

    .

---
+++ atividade02/script.js
@@ -0,0 +1,12 @@
+function verificar() {
+    var msg = window.document.getElementById ('msg')
+    var img = window.document.getElementById ('imagem')
+    var data = new Date()
+    var ano = data.getFullYear()
+    var tano = document.getElementById ('txtano')
+    var res = document.getElementById('res')
+
+    if(tano.Value.length)
+
+
+}
